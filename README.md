063f991dc3a6c322ac48d10a6b781ee86b280447
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-20 14:56:19 -0700

    Add basic project format

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 Summary reports of login.gov activity
 
+## Development
+
+### Run Development Server
+
+```bash
+npm install
+```
+
+### Run Tests
+
+```
+npm run test
+```
---
+++ src/report.test.js
@@ -0,0 +1,14 @@
+import { path } from "./report.js";
+import { expect } from "chai";
+
+describe("Report", () => {
+  describe("#path", () => {
+    it("formats a report as a path", () => {
+      const date = new Date('2021-12-01');
+
+      expect(path({ reportName: "some-report", date })).to.equal(
+        "/prod/some-report/2021/2021-12-01.some-report.json"
+      );
+    });
+  });
+});
