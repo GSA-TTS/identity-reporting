ed35e669b37c35e8f34fc11bd7ee5622bbc75c40
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-20 15:39:15 -0700

    Set up prettier

--- src/report.js
+++ src/report.js
@@ -1,24 +1,24 @@
-import { utcFormat, utcParse } from 'd3-time-format';
+import { utcFormat, utcParse } from "d3-time-format";
 
-const yearFormat = utcFormat('%Y');
-const yearMonthDayFormat = utcFormat('%Y-%m-%d');
+const yearFormat = utcFormat("%Y");
+const yearMonthDayFormat = utcFormat("%Y-%m-%d");
 
 /**
  * @typedef PathOptions
  * @prop {string} reportName
  * @prop {Date} date
- * @prop {string=} env 
+ * @prop {string=} env
  */
 
 /**
  * @param {PathOptions} options
  */
-function path({ reportName, date, env = 'prod' }) {
+function path({ reportName, date, env = "prod" }) {
   const year = yearFormat(date);
   const day = yearMonthDayFormat(date);
 
   // ex: /prod/daily-auths-report/2021/2021-07-27.daily-auths-report.json
-  return `/${env}/${reportName}/${year}/${day}.${reportName}.json`
+  return `/${env}/${reportName}/${year}/${day}.${reportName}.json`;
 }
 
 export { path };
--- src/report.test.js
+++ src/report.test.js
@@ -4,10 +4,10 @@ import { expect } from "chai";
 describe("Report", () => {
   describe("#path", () => {
     it("formats a report as a path", () => {
-      const date = new Date('2021-12-01');
+      const date = new Date("2021-12-01");
 
       expect(path({ reportName: "some-report", date })).to.equal(
-        "/prod/some-report/2021/2021-12-01.some-report.json"
+        "/prod/some-report/2021/2021-12-01.some-report.json",
       );
     });
   });
