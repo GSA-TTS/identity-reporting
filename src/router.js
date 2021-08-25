0b7ff5c50c3f813219ed3758e4a30eece807a3ee
Zach Margolis <zachary.margolis@gsa.gov>
2021-08-25 16:33:23 -0700

    Make a pass at adding types

--- src/router.js
+++ src/router.js
@@ -4,24 +4,51 @@ import { Router as BaseRouter, Link as BaseLink, route as baseRoute } from "prea
 
 const BASE_PATH = ((base) => (base != "/" ? base : undefined))(import.meta.env.BASE_URL);
 
+/**
+ * @param {string} path
+ * @returns {string}
+ */
 function getFullPath(path) {
   return [BASE_PATH, path].filter(Boolean).join("/");
 }
 
+/**
+ * @typedef RouterProps
+ * @property {import('preact').VNode[]} children
+ */
+
+/**
+ * @param {RouterProps} props
+ * @returns {import('preact').VNode}
+ */
 export function Router({ children }) {
   return html`
     <${BaseRouter}>
-      ${toChildArray(children).map((child) =>
-        cloneElement(child, { path: getFullPath(child.props.path) })
+      ${toChildArray(children).map(
+        (child) =>
+          typeof child === "object" && cloneElement(child, { path: getFullPath(child.props.path) })
       )}
     <//>
   `;
 }
 
+/**
+ * @typedef LinkProps
+ * @property {string} href
+ */
+
+/**
+ * @param {LinkProps} props
+ * @returns {import('preact').VNode}
+ */
 export function Link({ href, ...otherProps }) {
   return html`<${BaseLink} href=${getFullPath(href)} ...${otherProps} />`;
 }
 
+/**
+ * @param {string} path
+ * @returns {boolean=}
+ */
 export function route(path) {
   return baseRoute(getFullPath(path));
 }
