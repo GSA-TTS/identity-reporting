7093cd1af38b68621ae192e70bf9bae6cde0554c
Zach Margolis <zachmargolis@users.noreply.github.com>
2021-08-26 08:35:53 -0700

    Update router.test.js
    fix some typos

--- src/router.test.js
+++ src/router.test.js
@@ -9,13 +9,13 @@ describe("router", () => {
       expect(getFullPath(path, basePath)).to.eq("/some/nested/directory/foobar.html");
     });
 
-    it("does not add extra slashes with basePath is blank", () => {
+    it("does not add extra slashes when basePath is blank", () => {
       const path = "/foo/bar.html";
       const basePath = "";
       expect(getFullPath(path, basePath)).to.eq("/foo/bar.html");
     });
 
-    it("does not add the base path when its already there", () => {
+    it("does not add the base path when it is already there", () => {
       const path = "/some/prefix/foo/bar.html";
       const basePath = "/some/prefix/";
       expect(getFullPath(path, basePath)).to.eq(path);
