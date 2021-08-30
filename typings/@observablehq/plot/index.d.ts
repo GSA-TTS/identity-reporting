bcac91c25ea8102b5a1bf40191873f4fa6d1081e
Andrew Duthie <andrew.duthie@gsa.gov>
2021-08-30 08:40:44 -0400

    Disable no-explicit-any for observable plot defs

--- typings/@observablehq/plot/index.d.ts
+++ typings/@observablehq/plot/index.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "@observablehq/plot" {
   export function plot(args: any): HTMLElement;
   export function ruleY(args: any): any;
