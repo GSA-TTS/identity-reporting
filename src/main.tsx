077138cd70ad7cf9b21c1a186784bb92d1498350
Zach Margolis <zachary.margolis@gsa.gov>
2021-09-15 12:32:04 -0700

    Use identity-style-guide nav

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import "./css/style.scss";
 
 import { render } from "preact";
-import { banner, accordion } from "identity-style-guide";
+import { banner, accordion, navigation } from "identity-style-guide";
 import { Link } from "./router";
 import { Routes } from "./routes";
 
-[banner, accordion].forEach((component) => component.on());
+[banner, accordion, navigation].forEach((component) => component.on());
 
 render(
   <div>
@@ -15,23 +15,31 @@ render(
           <a href="/" title="Home" aria-label="Home">
             <img
               src="/node_modules/identity-style-guide/dist/assets/img/login-gov-logo.svg"
-              role="img"
               className="usa-logo__img"
               alt="Login.gov"
             />
             <em className="usa-logo__text">Data</em>
           </a>
         </div>
-        <button className="usa-menu-btn">Menu</button>
+        <button className="usa-menu-btn" type="button">
+          Menu
+        </button>
       </div>
       <nav className="usa-nav">
         <div className="usa-nav__inner">
+          <button className="usa-nav__close" type="button">
+            <img src="/node_modules/identity-style-guide/dist/assets/img/close.svg" alt="Close" />
+          </button>
           <ul className="usa-nav__primary usa-accordion">
             <li className="usa-nav__primary-item">
-              <Link href="/">Home</Link>
+              <Link href="/" activeClassName="usa-current">
+                Home
+              </Link>
             </li>
             <li className="usa-nav__primary-item">
-              <Link href="/daily-auths-report/">Daily Auths Report</Link>
+              <Link href="/daily-auths-report/" activeClassName="usa-current">
+                Daily Auths Report
+              </Link>
             </li>
           </ul>
         </div>
