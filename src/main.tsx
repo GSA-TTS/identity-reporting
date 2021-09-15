1b9c038f905443945c1fff1bc6cd4c9c523538e9
Zach Margolis <zachary.margolis@gsa.gov>
2021-09-15 08:58:13 -0700

    Style the nav, add login.gov logo

--- src/main.tsx
+++ src/main.tsx
@@ -9,14 +9,34 @@ import { Routes } from "./routes";
 
 render(
   <div>
-    <nav>
-      <div>
-        <Link href="/">Home</Link>
+    <header className="usa-header usa-header--extended">
+      <div className="usa-navbar">
+        <div className="usa-logo">
+          <a href="/" title="Home" aria-label="Home">
+            <img
+              src="/node_modules/identity-style-guide/dist/assets/img/login-gov-logo.svg"
+              role="img"
+              className="usa-logo__img"
+              alt="Login.gov"
+            />
+            <em className="usa-logo__text">Data</em>
+          </a>
+        </div>
+        <button className="usa-menu-btn">Menu</button>
       </div>
-      <div>
-        <Link href="/daily-auths-report/">Daily Auths Report</Link>
-      </div>
-    </nav>
+      <nav className="usa-nav">
+        <div className="usa-nav__inner">
+          <ul className="usa-nav__primary usa-accordion">
+            <li className="usa-nav__primary-item">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="usa-nav__primary-item">
+              <Link href="/daily-auths-report/">Daily Auths Report</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
     <main>
       <Routes />
     </main>
