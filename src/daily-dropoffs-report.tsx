5fd8a58e50399280bab9287c119bd14475843a8d
Zach Margolis <zachary.margolis@gsa.gov>
2021-09-22 23:17:27 -0700

    Line chart with mouseover

--- src/daily-dropoffs-report.tsx
+++ src/daily-dropoffs-report.tsx
@@ -1,15 +1,19 @@
 import { VNode } from "preact";
-import { useContext, useEffect } from "preact/hooks";
+import { useContext, useEffect, useRef, useState } from "preact/hooks";
 import { utcDays } from "d3-time";
 import { useQuery } from "preact-fetching";
 import { format } from "d3-format";
-import { group, ascending } from "d3-array";
+import { group, ascending, max } from "d3-array";
 import { AgenciesContext } from "./context/agencies-context";
 import Table, { TableData } from "./table";
 import { ReportFilterContext } from "./context/report-filter-context";
 import { path as reportPath } from "./report";
-import { scaleLinear } from "d3-scale";
+import { scalePoint, scaleLinear, scaleOrdinal } from "d3-scale";
 import { csvParse, autoType } from "d3-dsv";
+import { line as d3Line } from "d3-shape";
+import { select, selectAll } from "d3-selection";
+import { Axis as D3Axis, axisBottom, axisLeft } from "d3-axis";
+import { schemeCategory10 } from "d3-scale-chromatic";
 
 enum Mode {
   /**
@@ -124,9 +128,11 @@ function aggregate(rows: DailyDropoffsRow[]): DailyDropoffsRow[] {
 function tabulate({
   rows: results,
   filterAgency,
+  issuerColor,
 }: {
   rows?: DailyDropoffsRow[];
   filterAgency?: string;
+  issuerColor: (issuer: string) => string;
 }): TableData {
   const filteredRows = (results || []).filter((d) => !filterAgency || d.agency === filterAgency);
 
@@ -149,7 +155,10 @@ function tabulate({
 
     return [
       agency,
-      <span title={issuer}>{friendly_name}</span>,
+      <span title={issuer}>
+        <span style={`color: ${issuerColor(issuer)}`}>⬤ </span>
+        {friendly_name}
+      </span>,
       ...STEP_TITLES.flatMap(({ key }, idx) => {
         const count = row[key] || 0;
         let comparedToFirst = 1;
@@ -186,13 +195,127 @@ function tabulate({
   };
 }
 
+interface StepCount {
+  step: Step;
+  count: number;
+}
+
+function toStepCounts(row: DailyDropoffsRow): StepCount[] {
+  return STEP_TITLES.map(({ key }) => ({
+    step: key,
+    count: row[key],
+  }));
+}
+
+function Axis({ axis, transform }: { axis: D3Axis<any>; transform: string }): VNode {
+  const ref = useRef(null as SVGGElement | null);
+
+  useEffect(() => {
+    if (ref.current) {
+      select(ref.current).call(axis);
+    }
+  });
+
+  return <g ref={ref} transform={transform} />;
+}
+
+function LineChart({
+  data,
+  color,
+  width = 400,
+  height = 400,
+}: {
+  data: DailyDropoffsRow[];
+  color: (issuer: string) => string;
+  width?: number;
+  height?: number;
+}): VNode {
+  const margin = {
+    top: 20,
+    right: 50,
+    bottom: 50,
+    left: 50,
+  };
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+  const ref = useRef(null as SVGSVGElement | null);
+
+  const x = scalePoint()
+    .domain(STEP_TITLES.map(({ key }) => key))
+    .range([0, innerWidth]);
+
+  const y = scaleLinear()
+    .domain([0, max(data || [], (d) => d.welcome) as number])
+    .range([innerHeight, 0]);
+
+  const line: any = d3Line()
+    .x((d: any) => x(d.step) as number)
+    .y((d: any) => y(d.count) as number);
+
+  return (
+    <svg ref={ref} height={height} width={width}>
+      <Axis axis={axisLeft(y)} transform={`translate(${margin.left}, ${margin.top})`} />
+      <Axis
+        axis={axisBottom(x)}
+        transform={`translate(${margin.left}, ${margin.top + innerHeight})`}
+      />
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        {(data || []).map((row) => (
+          <path
+            d={line(toStepCounts(row))}
+            fill="none"
+            stroke={color(row.issuer)}
+            stroke-width="1"
+            onMouseEnter={() =>
+              ref.current &&
+              select(ref.current)
+                .selectAll(".dots")
+                .attr("hidden", function () {
+                  const elem = this as SVGGElement;
+                  return elem.dataset.issuer === row.issuer ? null : false;
+                })
+            }
+          />
+        ))}
+      </g>
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        {(data || []).map((row) => (
+          <g class="dots" hidden data-issuer={row.issuer}>
+            {toStepCounts(row).map(({ step, count }, idx, stepCounts) => {
+              let comparedToFirst = 0;
+              if (idx > 0) {
+                comparedToFirst = count / stepCounts[0].count;
+              }
+
+              return (
+                <>
+                  <circle cx={x(step)} cy={y(count)} r="3" fill={color(row.issuer)} />
+                  <text x={x(step)} y={y(count)} font-size="12" dx="3" dy="-3">
+                    <tspan x={x(step)}>{formatWithCommas(count)}</tspan>
+                    {idx > 0 && <tspan x={x(step)} dy="1.2em">(${formatAsPercent(comparedToFirst)})</tspan>}
+                  </text>
+                </>
+              );
+            })}
+          </g>
+        ))}
+      </g>
+    </svg>
+  );
+}
+
 function DailyDropffsReport(): VNode {
+  const ref = useRef(null as HTMLDivElement | null);
+  const [width, setWidth] = useState(undefined as number | undefined);
   const { setAgencies } = useContext(AgenciesContext);
   const { start, finish, agency, env } = useContext(ReportFilterContext);
 
   const { data } = useQuery(`${start.valueOf()}-${finish.valueOf()}`, () =>
     loadData(start, finish, env)
   );
+
+  const issuerColor = scaleOrdinal(schemeCategory10);
 
   useEffect(() => {
     if (!data) {
@@ -206,10 +329,20 @@ function DailyDropffsReport(): VNode {
     setAgencies(allAgencies);
   }, [data]);
 
+  useEffect(() => {
+    const listener = () => setWidth(ref.current?.offsetWidth);
+
+    listener();
+
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener);
+  });
+
   return (
-    <>
+    <div ref={ref}>
+      <LineChart data={data || []} width={width} color={issuerColor} />
       <Table
-        data={tabulate({ rows: data, filterAgency: agency })}
+        data={tabulate({ rows: data, filterAgency: agency, issuerColor })}
         numberFormatter={formatWithCommas}
       />
     </div>
