0d2c21cc1a9b3d358cf40b2316d5e36d8dad60b9
Zach Margolis <zachmargolis@users.noreply.github.com>
2022-06-17 12:07:39 -0700

    Update src/components/icon.tsx
    lowercase url
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,6 +1,6 @@
 import { VNode } from "preact";
 
-import SpriteURL from "../../node_modules/identity-style-guide/dist/assets/img/sprite.svg";
+import spriteURL from "../../node_modules/identity-style-guide/dist/assets/img/sprite.svg";
 
 interface IconProps {
   icon: string;
@@ -8,7 +8,7 @@ interface IconProps {
 export default function Icon({ icon }: IconProps): VNode {
   return (
     <svg className="usa-icon" aria-hidden="true" focusable="false" role="img">
-      <use href={`${SpriteURL}#${icon}`} />
+      <use href={`${spriteURL}#${icon}`} />
     </svg>
   );
 }
